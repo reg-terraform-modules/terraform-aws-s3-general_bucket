2900b67017b3477d2b284dad9ae3355e7130d530
Jørgen Walberg Bakke <walle.p@gmail.com>
2022-06-16 16:26:21 +0200

    changed cors

--- main.tf
+++ main.tf
@@ -16,16 +16,16 @@ resource "aws_s3_bucket_cors_configuration" "this"{
   count = var.include_cors ? 1 : 0
   bucket = aws_s3_bucket.this.bucket
 
-  dynamic "cors_rule" {
-    for_each = var.include_cors == true ? toset([1]) : toset([])
-    content {
-        allowed_headers = var.cors_rules["allowed_headers"]
-        allowed_methods = var.cors_rules["allowed_methods"]
-        allowed_origins = var.cors_rules["allowed_origins"]
-        expose_headers  = var.cors_rules["expose_headers"]
-        max_age_seconds = var.cors_rules["max_age_seconds"]
+  cors_rule {
+    for_each = var.cors_rules
+      content {
+          allowed_headers = var.cors_rules["allowed_headers"]
+          allowed_methods = var.cors_rules["allowed_methods"]
+         allowed_origins = var.cors_rules["allowed_origins"]
+         expose_headers  = var.cors_rules["expose_headers"]
+         max_age_seconds = var.cors_rules["max_age_seconds"]
+      }
     }
-  }
 }
 
 resource "aws_s3_bucket_versioning" "this" {
